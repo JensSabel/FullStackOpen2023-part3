bd3d0cc59e8403c18b4fb449599697937567482f
Jens Sabel <jens.sabel@abo.fi>
2024-01-12 16:18:03 +0200

    Exercise 3.4 Completed

--- index.js
+++ index.js
@@ -46,6 +46,13 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has info for ${length} people <br/> ${timeString} </p>`)
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`)
